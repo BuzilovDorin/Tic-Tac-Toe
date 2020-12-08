4f47b95fb1dc4a7dbe9efb666a16b50d15a93756
BuzilovDorin <buzilovdorin@gmail.com>
2020-12-08 20:51:49 +0000

    Added SFX

--- scripts.js
+++ scripts.js
@@ -1,9 +1,20 @@
 $(document).click(function (e) {
-    if ($(e.target).attr("class") != "none") {
+    if ($(e.target).attr("id") == "reset") {
+        $("li").empty()
+        $("li").removeClass()
+        $("li").addClass('none')
+        var soundbyte = document.createElement("audio")
+        soundbyte.src = "/Assets/reset.mp3"
+        soundbyte.play()
+    }
+    else if ($(e.target).attr("class") != "none") {
         return false
     }
     else {
         $(e.target).append('x')
-        $(e.target).toggleClass("x")
+        $(e.target).toggleClass("none x")
+        var soundbyte = document.createElement("audio")
+        soundbyte.src = "/Assets/click1.mp3"
+        soundbyte.play()
     }
 })
